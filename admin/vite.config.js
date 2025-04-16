51d71ac6fcf734f4d888a6faaf4210a20b6c3e08
nxhieu3102 <nguyenxuanhieuktb10@gmail.com>
2025-04-16 20:26:36 +0700

    update nginx

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -5,4 +5,21 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/admin/',
+  server: {
+    host: '0.0.0.0',
+    strictPort: true,
+    cors: true
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    manifest: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      }
+    }
+  }
 })
